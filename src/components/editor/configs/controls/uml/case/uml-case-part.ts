c4f3dca249603faabd8bb7c56ae829f68deac3a8
猴赛雷 <hoslay@163.com>
2023-12-21 15:02:45 +0800

    规划了UML中的各种图形

---
+++ src/components/editor/configs/controls/uml/case/uml-case-part.ts
@@ -0,0 +1,17 @@
+export default {
+  'id': '302004',
+  'name': '参与者',
+  'code': 'coop',
+  'desc': 'UML的用例图-协作',
+  'from': '100002',
+  'icon': 'toolbox-shape-circle',
+  'define': {
+    width: 110,
+    height: 70,
+    text: "<<actor>>\r\nSystem Actor",
+    textStyle: {
+      valign: 1,
+      italic: 1
+    }
+  }
+}
