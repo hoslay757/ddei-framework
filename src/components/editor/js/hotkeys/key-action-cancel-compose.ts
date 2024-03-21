b07cf1d518eff39b7ecc2fdefca34cc3d0c8b73b
猴赛雷 <hoslay@163.com>
2024-03-21 11:55:19 +0800

    快捷键取消组合改为调用buscommand

--- src/components/editor/js/hotkeys/key-action-cancel-compose.ts
+++ src/components/editor/js/hotkeys/key-action-cancel-compose.ts
@@ -15,29 +15,11 @@ class DDeiKeyActionCancelCompose extends DDeiKeyAction {
   action(evt: Event, ddInstance: DDei): void {
     //修改当前操作控件坐标
     if (ddInstance && ddInstance.stage) {
-      let stageRender = ddInstance.stage.render;
       //当前激活的图层
       let layer = ddInstance.stage.layers[ddInstance.stage.layerIndex]
       let selectedModels = layer.getSelectedModels();
       if (selectedModels.size > 0) {
-        let models = Array.from(selectedModels.values());
-        //添加元素到容器,并从当前layer移除元素
-        models.forEach(item => {
-          if (item.baseModelType == "DDeiContainer") {
-            if (item.models && item.models.size > 0) {
-              ddInstance.bus.push(DDeiEnumBusCommandType.ModelChangeContainer, { oldContainer: layer, models: [item] }, evt);
-              ddInstance.bus.push(DDeiEnumBusCommandType.ModelChangeSelect, { models: [item], value: DDeiEnumControlState.DEFAULT }, evt);
-              let models = Array.from(item.models.values());
-              ddInstance.bus.push(DDeiEnumBusCommandType.ModelChangeContainer, { newContainer: layer, oldContainer: item, models: models }, evt);
-              ddInstance.bus.push(DDeiEnumBusCommandType.ModelChangeSelect, { models: models, value: DDeiEnumControlState.SELECTED }, evt);
-            }
-          }
-        });
-        ddInstance.bus.push(DDeiEnumBusCommandType.NodifyChange);
-        ddInstance.bus.push(DDeiEnumBusCommandType.ClearTemplateVars, null, evt);
-        ddInstance.bus.push(DDeiEnumBusCommandType.AddHistroy, null, evt);
-        //渲染图形
-        ddInstance.bus.push(DDeiEnumBusCommandType.RefreshShape, null, evt);
+        ddInstance.bus.push(DDeiEnumBusCommandType.ModelCancelMerge);
         ddInstance.bus.executeAll();
       }
     }
