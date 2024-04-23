3c91f97e5704d98bcb80fd5901c4c4836f242768
wangyb <codowang@163.com>
2024-04-23 09:37:48 +0800

    修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import {resolve} from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    // minify: false,
+    minify: false,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.ts'),
+        main: resolve(__dirname, 'src/index.ts')
       },
+      external: ['vue', 'lodash', 'three']
     },
-    //设置打包的文件和名称，名称这里可以先去npm官网搜索一下是否存在，否则后面发包不成功也要修改  
+    //设置打包的文件和名称，名称这里可以先去npm官网搜索一下是否存在，否则后面发包不成功也要修改
     lib: {
-      entry: "./src/index.ts",
-      name: "ddei-framework"
-    },
+      entry: './src/index.ts',
+      name: 'ddei-framework'
+    }
   },
   resolve: {
     alias: {
       '@ddei-core': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
+  }
 })
